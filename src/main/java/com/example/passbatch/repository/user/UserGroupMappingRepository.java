967d0365457c36beb98f1b13bcf40505ce98bf0b
han <rudgksdl94@gmail.com>
2024-05-28 15:00:50 +0900

    유저 레포지토리, 엔티티, 유틸

---
+++ src/main/java/com/example/passbatch/repository/user/UserGroupMappingRepository.java
@@ -0,0 +1,8 @@
+package com.example.passbatch.repository.user;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserGroupMappingRepository extends JpaRepository<UserGroupMappingEntity, Integer> {
+    List<UserGroupMappingEntity> findByUserGroupId(String userGroupId);
+}
