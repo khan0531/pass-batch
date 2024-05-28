63244480c406c819bcab4e4f4ef2f0eca26661ca
han <rudgksdl94@gmail.com>
2024-05-28 15:12:23 +0900

    job 컨트롤러

---
+++ src/main/java/com/example/passbatch/controller/JobLauncherRequest.java
@@ -0,0 +1,22 @@
+package com.example.passbatch.controller;
+
+import java.util.Properties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+
+@Getter
+@Setter
+@ToString
+public class JobLauncherRequest {
+    private String name;
+    private Properties jobParameters;
+
+    public JobParameters getJobParameters() {
+        return new JobParametersBuilder(this.jobParameters).toJobParameters();
+
+    }
+
+}
