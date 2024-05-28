803783a13d8d2d825598e0c519742de6bd734a9e
han <rudgksdl94@gmail.com>
2024-05-28 15:01:56 +0900

    예약, 패키지, 패스 레포지토리

---
+++ src/main/java/com/example/passbatch/repository/pass/BulkPassRepository.java
@@ -0,0 +1,11 @@
+package com.example.passbatch.repository.pass;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BulkPassRepository extends JpaRepository<BulkPassEntity, Integer> {
+    // WHERE status = :status AND startedAt > :startedAt
+    List<BulkPassEntity> findByStatusAndStartedAtGreaterThan(BulkPassStatus status, LocalDateTime startedAt);
+
+}
